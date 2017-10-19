02817bff1629dd53fe77aeade8c3507921a05d14
Guy Bedford <guybedford@gmail.com>
2017-10-19 14:40:10 +0200

    ensure trailing slash handling

--- loader.mjs
+++ loader.mjs
@@ -8,14 +8,18 @@ const filePrefix = 'file://' + (isWindows ? '/' : '');
 const cache = {};
 
 module._nodeModulePaths = () => [];
-module._resolveFilename = (request,  parent, isMain) => {
+module._resolveFilename = (request, parent, isMain) => {
   if (request.match(winPathRegEx))
     request = '/' + request;
+  if (request[request.length - 1] === '/')
+    request = request.substr(0, request.length - 1);
   let { resolved } = jspmResolve.sync(request, parent && parent.filename, { cjsResolve: true, cache });
   return resolved;
 };
 
 export async function resolve (name, parentUrl) {
+  if (name[name.length - 1] === '/')
+    name = name.substr(0, name.length - 1);
   const { resolved, format } = await jspmResolve(name,
       decodeURI(parentUrl).substr(filePrefix.length), { cache });
   const url = filePrefix + encodeURI(resolved);
