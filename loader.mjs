59f64f893b112b57c92899f3da25ba24fac67f09
Guy Bedford <guybedford@gmail.com>
2017-11-15 15:20:29 +0200

    update loader for file extension handling

--- loader.mjs
+++ loader.mjs
@@ -13,7 +13,7 @@ module._resolveFilename = (request, parent, isMain) => {
     request = '/' + request;
   if (request[request.length - 1] === '/')
     request = request.substr(0, request.length - 1);
-  let { resolved } = jspmResolve.sync(request, parent && parent.filename, { cjsResolve: true, cache });
+  const { resolved } = jspmResolve.sync(request, parent && parent.filename, { cjsResolve: true, cache });
   return resolved;
 };
 
@@ -22,6 +22,8 @@ export async function resolve (name, parentUrl) {
     name = name.substr(0, name.length - 1);
   const { resolved, format } = await jspmResolve(name,
       decodeURI(parentUrl).substr(filePrefix.length), { cache });
+  if (format === undefined)
+    throw new Error(`Unable to load ${resolved}, as it does not have a valid module format file extension.`);
   const url = filePrefix + encodeURI(resolved);
   return { url, format };
 }
