7de281b1c04044fe0fd8a52e7415e873b92db46d
Guy Bedford <guybedford@gmail.com>
2019-04-18 01:33:43 +0200

    update harness

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 import Mocha from 'mocha';
-
-const tests = ['edge-cases.js', 'standard-cases.js'];
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { promises as fsPromises } from 'fs';
 
 (async () => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const tests = (await fsPromises.readdir(__dirname)).filter(name => name.endsWith('.js'));
   const mocha = new Mocha({ ui: 'tdd' });
 
   for (const test of tests) {
